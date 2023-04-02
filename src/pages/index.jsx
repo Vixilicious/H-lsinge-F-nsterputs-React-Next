174768e77f38e26607bae147b126c4b80c98b04f
Vixi <victoriaaskander@gmail.com>
2023-04-02 16:53:21 +0200

    meta information

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,6 +5,7 @@ import LandingPage from '@/Components/LandingPage/LandingPage';
 import Navbar from '@/Components/Navbar/Navbar';
 import Tjanster from '@/Components/Tjanster/Tjanster';
 import React, { useRef } from 'react';
+import Head from 'next/head';
 
 export default function App() {
   const tjansterRef = useRef(null);
@@ -12,19 +13,25 @@ export default function App() {
   const contactRef = useRef(null);
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', gap: 120 }}>
-      <div style={{ height: '100vh' }}>
-        <Navbar
-          tjansterRef={tjansterRef}
-          aboutRef={aboutRef}
-          contactRef={contactRef}
-        />
-        <LandingPage aboutRef={aboutRef} />
+    <div>
+      <Head>
+        <title>Alltid glasklart!</title>
+        <meta name='description' content='Det här är min sida' />
+      </Head>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: 120 }}>
+        <div style={{ height: '100vh' }}>
+          <Navbar
+            tjansterRef={tjansterRef}
+            aboutRef={aboutRef}
+            contactRef={contactRef}
+          />
+          <LandingPage aboutRef={aboutRef} />
+        </div>
+        <Tjanster ref={tjansterRef} />
+        <About ref={aboutRef} />
+        <Contact ref={contactRef} />
+        <Footer />
       </div>
-      <Tjanster ref={tjansterRef} />
-      <About ref={aboutRef} />
-      <Contact ref={contactRef} />
-      <Footer />
     </div>
   );
 }
